49d8161eb30b60bf0b6c35936d4a95cf2f378f7d
Dmitry <marsssvolta@gmail.com>
2021-12-01 12:10:06 +0400

    add stream

--- src/main/java/CSVFileHandler.java
+++ src/main/java/CSVFileHandler.java
@@ -3,14 +3,36 @@ import entities.Address;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CSVFileHandler extends FileHandler {
 
     @Override
     void readFile(String path) {
 
+        try (Stream<String> stream = Files.lines(Paths.get(path)).parallel()) {
+            System.out.println(Constants.PLEASE_WAIT);
+
+            addressList.addAll(stream.skip(1).map(this::createAddress).collect(Collectors.toList()));
+
+        } catch (FileNotFoundException e) {
+            System.out.println(Constants.FILE_NOT_FOUND);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*@Override
+    void readFile(String path) {
+
         try (var reader = new BufferedReader(new FileReader(path))) {
             System.out.println(Constants.PLEASE_WAIT);
+
             String line;
 
             reader.readLine();
@@ -25,12 +47,21 @@ public class CSVFileHandler extends FileHandler {
 
                 addressList.add(address);
             }
-
         } catch (FileNotFoundException e) {
             System.out.println(Constants.FILE_NOT_FOUND);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }*/
+
+    private Address createAddress(String line) {
+        String[] values = line.split(Constants.DELIMITER);
+        String city = values[0].replaceAll(Constants.REG_QUOTES, Constants.SIGN_EMPTY);
+        String street = values[1].replaceAll(Constants.REG_QUOTES, Constants.SIGN_EMPTY);
+        String house = values[2];
+        String floor = values[3];
+
+        return new Address(city, street, house, floor);
     }
 }
